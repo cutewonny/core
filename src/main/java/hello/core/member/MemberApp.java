b9fa31fe2d2f702c994e5a5719906034465cc1ab
cutewonny <cutewonny@ad-max.co.kr>
2022-05-06 15:03:40 +0900

    스프링컨테이너 사용

--- src/main/java/hello/core/member/MemberApp.java
+++ src/main/java/hello/core/member/MemberApp.java
@@ -1,12 +1,18 @@
 package hello.core.member;
 
 import hello.core.Order.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
 //        MemberService memberService = new MemberServiceImpl();
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);//이름, 타입
 
 
         Member member = new Member(1L, "워니", Grade.VIP);
